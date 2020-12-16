88b04a5aa2d7c71fef343f03ecd28f0006adad0a
nascbiel <bielnascsantos@gmail.com>
2020-12-16 16:34:24 -0300

    class emprestimo empresa

--- java/aulas/src/bancos/BancoTeste.java
+++ java/aulas/src/bancos/BancoTeste.java
@@ -1,10 +1,36 @@
 package bancos;
 
+import java.util.Scanner;
+
 public class BancoTeste {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		ContaEmpresa conta1 = new ContaEmpresa(1,10000.00);
+		Scanner teclado = new Scanner(System.in);
+		conta1.setCpf("555.333.888-22");
+		char simNao = 0;
 		
-
+		
+		conta1.credito(300);
+		conta1.debito(200);
+		System.out.println("O SALDO ATUAL:"+conta1.getSaldo());
+		System.out.println("O emprestimo da empresa é "+conta1.getEmprestimoEmpresa());
+		System.out.println("Deseja acessar o emprestimo?");
+		System.out.println("[S] para SIM   /   [N] para NÃO");
+		System.out.println("Digite o comando desejado:");
+		simNao=teclado.next().toUpperCase().charAt(0);
+		if(simNao=='S')
+		{
+			conta1.emprestar(200);
+		}
+		else if(simNao=='N')
+		{
+			System.out.println(conta1.getSaldo());
+		}
+		else {
+			System.out.println("DIGITO INVÁLIDO");
+		}
 	}
 
 }
--- java/aulas/src/bancos/Conta.java
+++ java/aulas/src/bancos/Conta.java
@@ -1,12 +1,48 @@
 package bancos;
 
-public class Conta {
+public abstract class Conta {
 
-	public int numeroConta;
-	public double saldo;
-	public String cpf;
+	private int numeroConta;
+	protected double saldo;
+	private String cpf;
+	
 	
 	public Conta(int numeroConta) {
 		this.numeroConta=numeroConta;
 	}
+
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+	
+	public void debito(double valorDebito) 
+	{
+		if(valorDebito>this.saldo) {
+			System.out.println("TRANSAÇÃO INVÁLIDA");
+		}
+		else {
+			this.saldo = this.saldo - valorDebito;
+			System.out.println("SALDO DA TRANSAÇÃO: "+this.saldo);
+		}
+	}
+	
+	public void credito (double valorCredito) 
+	{
+		this.saldo = this.saldo + valorCredito;
+	}
+	
+	
 }
