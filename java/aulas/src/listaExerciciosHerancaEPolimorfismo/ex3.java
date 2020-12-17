e7552db6f5a5914c3cceafc35b7197a2e70f45e1
nascbiel <bielnascsantos@gmail.com>
2020-12-17 17:53:00 -0300

    atualizando o exercicio 3 de list

--- java/aulas/src/listaExerciciosHerancaEPolimorfismo/ex3.java
+++ java/aulas/src/listaExerciciosHerancaEPolimorfismo/ex3.java
@@ -4,16 +4,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class ex3 {
-	
-	/*
-	 * Crie uma um programa para trabalhar com estoque de uma loja, 
-	 * o programa deverá trabalhar com Collection do tipo List do Java para manipular os dados desse estoque, 
-	 * o programa deverá atender as seguintes funcionalidades:
-	 * Armazenar dados da List
-		Atualizar dados da list.
-	
-	 * 
-	 */
 		public static void main(String[] args) {
 			
 			Collection<String> nomes = new ArrayList();
